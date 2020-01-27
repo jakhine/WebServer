fba1b4d049c3e6eb6c366b1e24c86a798f0b1a8d
jakhine <jakhine@gmail.com>
2020-01-27 19:49:23 +0300

    В main методе создан экземпляр Http сервера с сокетом,  контекстом.  Добавлен класс HttpHandlerImpl с простой логикой для обработки запросов.

--- src/main/java/MySimpleServer.java
+++ src/main/java/MySimpleServer.java
@@ -1,10 +1,20 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
 public class MySimpleServer {
 
     public static void main(String[] args) {
-
-
-
-
+        try {
+            InetSocketAddress inetSocketAddress = new InetSocketAddress("localhost", 80); //создаем соккет
+            HttpServer server = HttpServer.create(inetSocketAddress,0); //создаем сервер и привязываем к нему сокет, (вторым аргументом - максимальное кол-во соединений)
+            HttpContext context =  server.createContext("/"); // создаем так называемый контекст
+            context.setHandler(new HttpHandlerImpl());  //обработчик запросов
+            server.start(); // запускаем сервер
+    }
+        catch (Exception e ){
+                                System.out.println(e);
+                             }
     }
 
 }
