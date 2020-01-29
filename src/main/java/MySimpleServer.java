933559278c27edd776a1a26bc50c2c272f9607b6
jakhine <jakhine@gmail.com>
2020-01-29 18:17:46 +0300

    создан метод для изменения настроек сервера. попытки разобраться как работает маппинг handler'а

--- src/main/java/MySimpleServer.java
+++ src/main/java/MySimpleServer.java
@@ -1,4 +1,5 @@
 import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
@@ -10,22 +11,32 @@ public class MySimpleServer {
     private static String rootCatalog = "/";
     private static String hostname = "localhost";
     private static int port = 80;
+    private static HttpServer server;
+    private static HttpContext rootContext;
+    private static HttpHandler httpHandler =  new MyHttpHandlerImpl();
 
 
     public static void main(String[] args) {
         try {
             InetSocketAddress inetSocketAddress = new InetSocketAddress(hostname, port); //создаем соккет
-            HttpServer server = HttpServer.create(inetSocketAddress,0); //создаем сервер и привязываем к нему сокет, (вторым аргументом - максимальное кол-во соединений)
-            HttpContext rootContext =  server.createContext(rootCatalog); // создаем так называемый контекст, адрес
-            rootContext.setHandler(new HttpHandlerImpl());  // привязываем обработчик запросов по этому адресу
+            server = HttpServer.create(inetSocketAddress,0); //создаем сервер и привязываем к нему сокет, (вторым аргументом - максимальное кол-во соединений)
+            rootContext =  server.createContext(rootCatalog); // создаем так называемый контекст, адрес
+            System.out.println(" rootContext.getPath() = " + rootContext.getPath());
 
-
-
+            rootContext.setHandler(httpHandler);  // привязываем обработчик запросов по этому адресу
             server.start(); // запускаем сервер
+            System.out.println("Сервер " + server.getAddress() + " запущен");
+
     }
         catch (Exception e ){
                                 System.out.println(e);
                              }
+    }
+    public static void configureServer (String newRootCatalog){
+        //TODO
+//        server.removeContext(rootCatalog);
+//        rootContext = server.createContext(newRootCatalog);
+
     }
 
 
