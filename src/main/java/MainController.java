e9c0e203b79619e47e101b86cf9964ff2f839dbe
jakhine <jakhine@gmail.com>
2020-02-05 17:22:03 +0300

    добавлена логика обработки запросов

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -13,32 +13,50 @@ public class MainController {
     public static void main(String[] args) {
         BasicConfigurator.configure(); //TODO донастроить log4j
         try {
-            MyServer mss = new MyServer().configureFromFile(configFile);
-            log.info(String.format("Server was created with the following parameters %s ", mss));
-            mss.createSocket();
+            MyServer myServer = new MyServer().configureFromFile(configFile);
+            log.info(String.format("Server was created with the following parameters %s ", myServer));
+            myServer.createSocket();
 
             while (true) {
-                Socket clientSocket = mss.listen();
+                Socket clientSocket = myServer.listen();
                 Request request = new Request(clientSocket);
                 if (request.path == null) continue;
                 log.info(String.format("Connection successful. from address -  %s , and with request - %s", clientSocket.getInetAddress(), request));
                 OutputStream output = clientSocket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
                 String path = request.path;
+
                 if (path.endsWith("/")) {
                     path = path + "index.html";
-                    File file = new File(mss.getRootFolderPath() + path);
-                    sendResponse(new Response(request), writer);
+                }
+                File file = new File(myServer.getRootFolderPath() + path);
+                Response response = new Response();
+
+                if (!file.exists()){
+                    response.setStatusCode("404 NOT FOUND");
+                    sendResponse(response, writer);
+
+                }
+
+                if (file.getPath().endsWith(".html")||file.getPath().endsWith(".txt")) {
+                    response.setStatusCode("200 OK");
+                    response.setHeaders("Content-Type: ", "text/html");
+                    sendResponse(response, writer);
                     sendTextHtml(file, writer);
                 }
-                else if (path.endsWith(".jpg")){
-                    sendResponse(new Response(request), writer);
-                    File file = new File(mss.getRootFolderPath() + path);
+
+                if (file.getPath().endsWith(".jpg")) {
+                    response.setStatusCode("200 OK");
+                    response.setHeaders("Content-Type: ", "image/jpeg");
+                    sendResponse(response, writer);
                     sendJpg(file, output);
                 }
 
-
-
+                if (file.isDirectory()){
+                    response.setStatusCode("501 Not Implemented");                  // "501 Not Implemented"
+                    sendResponse(response,writer);
+                    file.list();
+                }
 
                 output.flush();
                 writer.close();
@@ -49,23 +67,21 @@ public class MainController {
         }
     }
 
+    static void sendResponse(Response response, PrintWriter writer) {
+        writer.println(response.getHeadLine());
+        writer.println(response.getHeaders().entrySet());      //; charset=utf-8"
+        log.info(String.format("headers %s Sent", response.getHeaders()));
+        writer.println();                               // пустая строка, сигнализирующая об окончании контента запроса
+    }
+
     static void sendTextHtml(File file, PrintWriter writer) throws IOException {
         FileReader fileReader = new FileReader(file);
-        log.info(file.getPath());
         char[] buf = new char[256];
         int count = 0;
         while ((count = fileReader.read(buf)) != -1) { //читает посимвольно из файла и пишет в буферный массив символов, возвращает ол-во символов или  -1 когда файл "кончился"
             writer.write(buf, 0, count);
         }
-
-        log.info("file Sent");
-    }
-
-    static void sendResponse(Response response, PrintWriter writer) {
-        writer.println("HTTP/1.1 200"); //TODO дописать заполнение заголовков ответа
-        writer.println("Content-Type: text/html");      //; charset=utf-8"
-        log.info("headers Sent");
-        writer.println();                               // пустая строка, сигнализирующая об окончании контента запроса
+        log.info(String.format("file %s Sent", file.getPath()));
     }
 
     static void sendJpg(File file, OutputStream output) throws IOException {
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,12 +1,32 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Response {
 
-    String protocol;
-    String statusCode;
-    Map<String, String> headers;
+   private String protocol = "HTTP/1.1";
+    private String statusCode;
+    private Map<String, String> headers = new HashMap<>();
 
-    public Response(Request request) {
-            //TODO
+    public Response() {
+
+    }
+
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+            }
+
+
+    public String getHeadLine() {
+        return  String.format("%s %s" ,protocol, statusCode);
+    }
+
+
+    public void setHeaders(String key, String value) {
+        headers.put(key, value);
+           }
+
+    public Map<String, String> getHeaders() {
+        return headers;
     }
 }
