4c689cc24a517af1287d65dc26f745129653813c
jakhine <jakhine@gmail.com>
2020-02-05 13:47:43 +0300

    добавлены методы для отправки файлов

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -10,7 +10,6 @@ public class MainController {
     public static Logger log = Logger.getLogger(MainController.class);
     public static File configFile = new File("C:\\Users\\A650322\\IdeaProjects\\WebServer\\src\\main\\resources\\config.yml"); //определяем файл с конфигами сервера
 
-
     public static void main(String[] args) {
         BasicConfigurator.configure(); //TODO донастроить log4j
         try {
@@ -22,58 +21,61 @@ public class MainController {
                 Socket clientSocket = mss.listen();
                 Request request = new Request(clientSocket);
                 if (request.path == null) continue;
-
                 log.info(String.format("Connection successful. from address -  %s , and with request - %s", clientSocket.getInetAddress(), request));
-
-
                 OutputStream output = clientSocket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
                 String path = request.path;
-
-
-                writer.println("HTTP/1.1 200 OK"); //TODO дописать заполнение заголовков ответа
-                writer.println("Content-Type: text/html");      //; charset=utf-8"
-                writer.println();                               // пустая строка, сигнализирующая об окончании контента запроса
-                //
                 if (path.endsWith("/")) {
                     path = path + "index.html";
                     File file = new File(mss.getRootFolderPath() + path);
-
-                    FileReader fileReader = new FileReader(file);
-                    char[] buf = new char[256];
-                    int count = 0;
-                    while ((count = fileReader.read(buf)) != -1) { //читает посимвольно из файла и пишет в буферный массив символов, возвращает ол-во символов или  -1 когда файл "кончился"
-                        writer.write(buf, 0, count);
-                    }
+                    sendResponse(new Response(request), writer);
+                    sendTextHtml(file, writer);
                 }
+                else if (path.endsWith(".jpg")){
+                    sendResponse(new Response(request), writer);
+                    File file = new File(mss.getRootFolderPath() + path);
+                    sendJpg(file, output);
+                }
+
+
+
+
+                output.flush();
                 writer.close();
                 clientSocket.close();
-
-
-                log.info(String.format("address -  %s ", request.path));
-                //TODO
             }
-
-
         } catch (IOException e) {
             log.error(String.format("Could not create and start web server: %s", e.getMessage()), e);
         }
-
-
-//        InetSocketAddress inetSocketAddress = new InetSocketAddress(hostname, port); //создаем соккет
-//        server = HttpServer.create(inetSocketAddress, 0); //создаем сервер и привязываем к нему сокет, (вторым аргументом - максимальное кол-во соединений)
-//        rootContext = server.createContext(rootFolder); // создаем так называемый контекст, адрес
-//        System.out.println(" rootContext.getPath() = " + rootContext.getPath());
-//
-//        rootContext.setHandler(httpHandler);  // привязываем обработчик запросов по этому адресу
-//        server.start(); // запускаем сервер
-//        log.info(String.format("Сервер %s запущен", server.getAddress()));
     }
 
-    public static String getRequestAddress(Socket clientSocket) throws IOException {
-        InputStream input = clientSocket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        return reader.readLine();
+    static void sendTextHtml(File file, PrintWriter writer) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        log.info(file.getPath());
+        char[] buf = new char[256];
+        int count = 0;
+        while ((count = fileReader.read(buf)) != -1) { //читает посимвольно из файла и пишет в буферный массив символов, возвращает ол-во символов или  -1 когда файл "кончился"
+            writer.write(buf, 0, count);
+        }
+
+        log.info("file Sent");
     }
 
+    static void sendResponse(Response response, PrintWriter writer) {
+        writer.println("HTTP/1.1 200"); //TODO дописать заполнение заголовков ответа
+        writer.println("Content-Type: text/html");      //; charset=utf-8"
+        log.info("headers Sent");
+        writer.println();                               // пустая строка, сигнализирующая об окончании контента запроса
+    }
+
+    static void sendJpg(File file, OutputStream output) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        System.out.println(file.getPath());
+        byte[] buf = new byte[250];
+        int count = 0;
+        while ((count = fileInputStream.read(buf)) != -1) {
+            output.write(buf, 0, count);
+        }
+
+    }
 }
--- src/main/java/Response.java
+++ src/main/java/Response.java
@@ -1,3 +1,12 @@
+import java.util.Map;
+
 public class Response {
 
+    String protocol;
+    String statusCode;
+    Map<String, String> headers;
+
+    public Response(Request request) {
+            //TODO
+    }
 }
